89bc6be2ca7888fadef32209cb62ab3ce152b63e
Eryx <evorui@gmail.com>
2020-12-06 13:42:42 +0800

    [IMP] support proxy websocket

--- main.go
+++ main.go
@@ -53,6 +53,9 @@ upstream %s {
         proxy_set_header    Host             $host;
         proxy_set_header    X-Real-IP        $remote_addr;
         proxy_set_header    X-Forwarded-For  $proxy_add_x_forwarded_for;
+        proxy_http_version  1.1;
+        proxy_set_header    Upgrade          $http_upgrade;
+        proxy_set_header    Connection       $connection_upgrade;
     }`
 	ngx_location_redirect_http_tpl = `
     location %s {
